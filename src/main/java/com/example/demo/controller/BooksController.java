ff18b006afbe630db5bcd76efcd44ebdf3d7691f
Aleksandr Kokhanovskyi <sasha.kohanovsky@gmail.com>
2023-06-18 14:22:42 +0300

    Break project

--- src/main/java/com/example/demo/controller/BooksController.java
+++ src/main/java/com/example/demo/controller/BooksController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BooksController {
 
     @GetMapping("/book")
-    public String getBook(){
+    public String getBook(){klsdmflkasmdf
         System.out.println();
         return "Kolobok";
     }
